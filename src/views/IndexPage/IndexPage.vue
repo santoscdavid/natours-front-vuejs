473177d6ea58522947221ac3152ea84633b6fa82
DSantos69 <tecnico.davidcavlcante@gmail,com>
2022-04-21 19:20:03 -0300

    #3 css converter initially to sass (in progress)

--- src/views/IndexPage/IndexPage.vue
+++ src/views/IndexPage/IndexPage.vue
@@ -1,27 +1,13 @@
 <template>
-  <Header></Header>
+    <Header></Header>
 </template>
 
 <script>
-import Header from "@/components/Header.vue";
+import Header from '@/components/Header.vue';
 export default {
-  name: "IndexPage",
-  components: { Header },
+    name: 'IndexPage',
+    components: { Header }
 };
 </script>
 
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-body {
-  font-family: "Lato", sans-serif;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 1.7;
-  color: #777;
-}
-</style>
+<style src="@/styles/base/_base.scss" lang="scss"></style>
