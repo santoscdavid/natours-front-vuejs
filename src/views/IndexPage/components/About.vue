1680f31575a6ed4ae7fa170fcb9160481fa8814b
DSantos69 <david.csantos69@gmail.com>
2022-04-29 09:44:35 -0300

    #5 initial structure of the about section (in progress)

---
+++ src/views/IndexPage/components/About.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="section-about">
+        <div class="u-center-text u-margin-bottom-8">
+            <h2 class="heading-secondary">Exciting tours for adventurous people</h2>
+        </div>
+        <div class="row">
+            <div class="col-1-of-2">Text Content</div>
+            <div class="col-1-of-2">Image composition</div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'About'
+};
+</script>
+
+<style></style>
