d2ff33867fd68b89726c989b9fcc9829b9b4e0d1
DSantos69 <david.csantos69@gmail.com>
2022-04-30 11:09:55 -0300

    #5 Added informational text from the site in the section about (in progress)

--- src/views/IndexPage/components/About.vue
+++ src/views/IndexPage/components/About.vue
@@ -1,10 +1,24 @@
 <template>
     <section class="section-about">
-        <div class="u-center-text u-margin-bottom-8">
+        <div class="u-center-text u-margin-bottom-big">
             <h2 class="heading-secondary">Exciting tours for adventurous people</h2>
         </div>
         <div class="row">
-            <div class="col-1-of-2">Text Content</div>
+            <div class="col-1-of-2">
+                <h3 class="heading-tertiary u-margin-bottom-small">You're going to fall in love with nature</h3>
+                <p class="paragraph">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto delectus inventore ad illo
+                    voluptatibus ipsam amet ab tempore totam veniam doloribus, neque sapiente deserunt, saepe velit
+                    itaque tenetur aspernatur at!
+                </p>
+                <h3 class="heading-tertiary u-margin-bottom-small">Live adventures like you never have before</h3>
+                <p class="paragraph">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto delectus inventore ad illo
+                    voluptatibus ipsam amet ab tempore totam veniam doloribus, neque sapiente deserunt, saepe velit
+                    itaque tenetur aspernatur at!
+                </p>
+                <a href="#" class="btn-text">Learn more &rarr;</a>
+            </div>
             <div class="col-1-of-2">Image composition</div>
         </div>
     </section>
