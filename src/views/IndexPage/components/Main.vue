a01ce979a768a62b7cc0d8343ffff6df8bfe0172
DSantos69 <david.csantos69@gmail.com>
2023-02-13 18:26:52 -0300

    added updates that have been stalled

--- src/views/IndexPage/components/Main.vue
+++ src/views/IndexPage/components/Main.vue
@@ -2,15 +2,17 @@
     <div>
         <About />
         <Features />
+        <Tours />
     </div>
 </template>
 
 <script>
 import About from './About.vue';
 import Features from './Features.vue';
+import Tours from './Tours.vue';
 export default {
     name: 'Main',
-    components: { About, Features }
+    components: { About, Features, Tours }
 };
 </script>
 
